02fada6e6e9b2bc51fdece06c722bc6e645e50de
Marco Bakera <marco@bakera.de>
2017-05-06 23:44:16 +0200

    basic output of some values.

--- tbs1-webuntis.py
+++ tbs1-webuntis.py
@@ -1,1 +1,23 @@
 import webuntis
+import configparser
+
+# from https://python-webuntis.readthedocs.io/en/latest/
+
+# read credentials from config file
+config = configparser.ConfigParser()
+config.read("config.ini")
+cred = config["credentials"]
+
+# create session object
+s = webuntis.Session(
+    username=cred["username"],
+    password=cred["password"],
+    server='cissa.webuntis.com',
+    useragent="WebUntis Test",
+    school='bk-bochum').login()
+
+for k in s.klassen():
+    print(k.name)
+
+    
+s.logout()
